d83548f1adb0f40f94fa1045f5ae36a87b103482
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2024-03-30 20:13:09 +0900

    Feat: Create Description component

---
+++ src/components/common/Description.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Description = ({ text, ...props }) => {
+  return <Text {...props}>{text}</Text>;
+};
+
+export default Description;
+
+const Text = styled.p`
+  white-space: pre-wrap;
+  text-align: center;
+  font-size: 18px;
+  line-height: 30px;
+`;
